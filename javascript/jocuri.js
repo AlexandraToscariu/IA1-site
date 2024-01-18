9a1255a13130466d92001a93cebc9b563f0c46d4
Toscariu Alberto Ionut <alberto.toscariu@icis.com>
2024-01-18 17:12:00 +0200

    fixat jocuri

--- javascript/jocuri.js
+++ javascript/jocuri.js
@@ -1,45 +1,36 @@
-// Get the DOM elements and initialize the game
-const input = document.querySelector("input"),
-  guess = document.querySelector(".guess"),
-  checkButton = document.querySelector("button"),
-  remainChances = document.querySelector(".chances");
-// Set the focus on input field
+const input = document.querySelector("input");
+const constguess = document.querySelector(".guess");
+const checkButton = document.querySelector("button");
+const remainChances = document.querySelector(".chances");
+
 input.focus();
+
 let randomNum = Math.floor(Math.random() * 100);
-chance = 10;
-// Listen for the click event on the check button
+let chance = 10;
+
 checkButton.addEventListener("click", () => {
-  // Decrement the chance variable on every click
   chance--;
-  // Get the value from the input field
   let inputValue = input.value;
-  // Check if the input value is equal to the random number
+
   if (inputValue == randomNum) {
-    // Update guessed number, disable input, check button text and color.
-    [guess.textContent, input.disabled] = ["Congratulations", true];
-    [checkButton.textContent, guess.style.color] = ["Replay", "#333"];
-    //Check if input value is > random number and within 1-99 range.
+
+    [guess.textContent, input.disabled] = ["Felicitări", true];
+    [checkButton.textContent, guess.style.color] = ["Să mai jucăm!", "#333"];
+
   } else if (inputValue > randomNum && inputValue < 100) {
-    // Update the guess text and remaining chances
-    [guess.textContent, remainChances.textContent] = ["Your guess is high", chance];
+
+    [guess.textContent, remainChances.textContent] = ["Numărul este mai mic!", chance];
     guess.style.color = "#333";
-    //Check if input value is < random number and within 1-99 range.
   } else if (inputValue < randomNum && inputValue > 0) {
-    // Update the guessed number text and remaining chances
-    [guess.textContent, remainChances.textContent] = ["Your guess is low", chance];
+    [guess.textContent, remainChances.textContent] = ["Numărul este mai mare!", chance];
     guess.style.color = "#333";
-    // If the input value is not within the range of 1 to 99
   } else {
-    // Update the guessed number text, color and remaining chances
-    [guess.textContent, remainChances.textContent] = ["Your number is invalid", chance];
+    [guess.textContent, remainChances.textContent] = ["Nu ai ales un număr!", chance];
     guess.style.color = "#DE0611";
   }
-  // Check if the chance is zero
   if (chance == 0) {
-    //Update check button, disable input, and clear input value.
-    // Update guessed number text and color to indicate user loss.
-    [checkButton.textContent, input.disabled, inputValue] = ["Replay", true, ""];
-    [guess.textContent, guess.style.color] = ["You lost the game", "#DE0611"];
+    [checkButton.textContent, input.disabled, inputValue] = ["Să mai jucăm!", true, ""];
+    [guess.textContent, guess.style.color] = ["Ai pierdut!", "#DE0611"];
   }
   if (chance < 0) {
     window.location.reload();
